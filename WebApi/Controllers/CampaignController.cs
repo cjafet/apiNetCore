f7b03c34817cbf91a187bc4778a5ea0ba0d7ebb8
Carlos Jafet Neto <cjafet07@gmail.com>
2018-03-24 10:32:22 -0300

    Add support to post stream through AWS Kinesis

--- WebApi/Controllers/CampaignController.cs
+++ WebApi/Controllers/CampaignController.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Amazon.Kinesis;
+using Amazon.Kinesis.Model;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using WebApi.Models;
+using System.IO;
+using System.Dynamic;
 
 namespace WebApi.Controllers
 {
@@ -24,6 +30,35 @@ namespace WebApi.Controllers
         {
             List<CampaignDto> campaigns = context.Campaign.ToList();
             return Json(new { campaigns });
+        }
+
+        [HttpPost()]
+        public void Post([FromBody]dynamic value)
+        {
+            string dataAsJson = JsonConvert.SerializeObject(value);
+            byte[] dataAsBytes = Encoding.UTF8.GetBytes(dataAsJson);
+            using (MemoryStream memoryStream = new MemoryStream(dataAsBytes))
+            {
+                try
+                {
+                    AmazonKinesisConfig config = new AmazonKinesisConfig();
+                    config.RegionEndpoint = Amazon.RegionEndpoint.USEast1;
+                    AmazonKinesisClient kinesisClient = new AmazonKinesisClient(config);
+                    String kinesisStreamName = "click-stream";
+
+                    PutRecordRequest requestRecord = new PutRecordRequest();
+                    requestRecord.StreamName = kinesisStreamName;
+                    requestRecord.PartitionKey = "temp";
+                    requestRecord.Data = memoryStream;
+
+                    kinesisClient.PutRecordAsync(requestRecord);
+                    //Console.WriteLine("Successfully sent record {0} to Kinesis. Sequence number: {1}", wt.Url, responseRecord.SequenceNumber);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to send record to Kinesis. Exception: {0}", ex.Message);
+                }
+            }
         }
             
     }
